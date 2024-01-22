9ebe4490745fa265b502aa84fef2b2e2abeb6780
Sagar Acharya <sagar_acharya@live.in>
2024-01-22 20:51:11 +0530

    basic setup and created counter component

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,13 @@
+<div>
+    {{-- Stop trying to control. --}}
+    <div>
+        <span>{{$count}}</span>
+        <button wire:click="increment">+</button>
+        <button wire:click="decrement">-</button>
+    </div>
+
+    <div>
+
+    </div>
+</div>
+
